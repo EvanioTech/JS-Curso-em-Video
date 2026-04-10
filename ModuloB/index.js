84710b7bdb2861e88a66bbe08fdf37fd992271ac
Evanio Tech <evanioshark@gmail.com>
2026-04-10 16:05:49 -0300

    entendendo variaveis

---
+++ ModuloB/index.js
@@ -0,0 +1,9 @@
+var aluno = "Josias" // vaza de escopo 
+let funcionario = "Alan" // variavel
+const jogador = "Toin" // constante
+
+typeof jogador // define o tipo
+
+
+console.log(aluno + funcionario + jogador)
+console.log(typeof jogador)
