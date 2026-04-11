78c427b6cbefe69100c35068ce5bb908ad6407d7
Evanio Tech <evanioshark@gmail.com>
2026-04-11 20:37:15 -0300

    aula condições parte 1

---
+++ ModuloD/index.js
@@ -0,0 +1,9 @@
+let cliente = "Amélia"
+let age = 18
+
+if (age >= 18) {
+     console.log(`A cliente ${cliente} pode comprar o que quiser.`)
+}
+else{
+   console.log(`A Cliente ${cliente} não pode comprar todos os produtos da loja`) 
+}
