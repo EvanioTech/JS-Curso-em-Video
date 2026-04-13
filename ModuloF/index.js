1c010f7d63b45c5202fa332ad4e76b81c4a5e9fa
Evanio Tech <evanioshark@gmail.com>
2026-04-13 15:22:13 -0300

    Entendo array

--- ModuloF/index.js
+++ ModuloF/index.js
@@ -1,6 +1,25 @@
 let aluno1 = []
 aluno1.push(1)
 aluno1.push(9)
-aluno1.push(10)
+aluno1.push(8)
+aluno1.push(7)
+aluno1.push(6)
+aluno1.push(5)
+aluno1.push(4)
 
 console.log(aluno1)
+
+aluno1.sort()
+console.log(aluno1)
+
+for (let index = 0; index < aluno1.length; index++) {
+    console.log( 'a posição ' + index + ' tem o valor de ' +aluno1[index])
+    
+}
+
+for( let i in aluno1) {
+    console.log( 'a posição ' + i + ' tem o valor de ' +aluno1[i])
+}
+
+let pos = aluno1.indexOf(9)
+console.log(pos)
